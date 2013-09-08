552249b6561918ecc42f0e3ab92cbbf0dd7af782
Kyle Burton <kyle.burton@gmail.com>
2013-09-08 10:55:49 -0400

    support lein2, test on clojure 1.2 through 1.5

--- project.clj
+++ project.clj
@@ -1,8 +1,22 @@
 (defproject com.github.kyleburton/clj-lfsr "1.3.1"
-  :description "LFSR Library"
+  :description        "LFSR Library"
+  :url                "http://github.com/kyleburton/clj-lfsr"
   :warn-on-reflection true
-  :dependencies
-    [ [org.clojure/clojure "1.3.0"] ]
-  :dev-dependencies 
-    [ [swank-clojure "1.4.0"] ])
-
+  :lein-release         {:deploy-via :clojars}
+  :license              {:name "Eclipse Public License - v 1.0"
+                         :url "http://www.eclipse.org/legal/epl-v10.html"
+                         :distribution :repo
+                         :comments "Same as Clojure"}
+  :repositories         {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
+  :local-repo-classpath true
+  :profiles             {:dev {:dependencies [[swank-clojure "1.4.3"]
+                                              [org.clojure/clojure "1.5.1"]]}
+                         ;; NB: the use of ex-info prevents 1.3 from being supported
+                         :1.2 {:dependencies [[org.clojure/clojure "1.2.0"]]}
+                         :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+                         :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+                         :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+                         :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
+  :aliases              {"all" ["with-profile" "1.2:1.3:1.4:1.5:1.6"]}
+  :global-vars          {*warn-on-reflection* true}
+  :dependencies         [])
